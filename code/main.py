8d95683141515927b79d5b8be4d9610491c2d4ba
Ashwin Vaswani <ashwin.vaswani99@gmail.com>
2019-11-14 02:35:33 +0530

    minor changes

--- code/main.py
+++ code/main.py
@@ -1,16 +1,11 @@
-# from pynput.keyboard import Key,Controller
 import os
-
 import numpy as np
 import cv2
 import time
 from keras.models import Model, load_model
 import subprocess
-# import pyautogui
 
 def minimizer():
-    # time.sleep(0.2)
-
     #For Windows
     if os.name == 'nt':
         import keyboard
@@ -28,7 +23,6 @@ def minimizer():
 
 
 def tabs_cycle():
-
     #For Windows
     if os.name == 'nt':
         import keyboard
@@ -45,8 +39,6 @@ def tabs_cycle():
         return "success"
 
 def bring_up_tabs():
-#     time.sleep(3)
-
     #For Windows
     if os.name == 'nt':
         import keyboard
@@ -63,8 +55,6 @@ def bring_up_tabs():
         return "success"
 
 def bring_down_tabs():
-#     time.sleep(0.5)
-
     #For Windows
     if os.name == 'nt':
         import keyboard
@@ -101,105 +91,120 @@ def take_ss(count=[0]):
         return "success"
 
 def refresher():
-    try:
-        keyboard.press_and_release('ctrl+r')
-    except Exception as e:
-        return str(e)
-    return "success"
+    # For windows
+    if os.name == 'nt':
+        import keyboard
+        try:
+            keyboard.press_and_release('ctrl+r')
+        except Exception as e:
+            return str(e)
+        return "success"
+    else:
+        try:
+            subprocess.call(["xte","keydown Ctrl_L","key r","keyup Ctrl_L"])
+        except Exception as e:
+            return str(e)
+        return "success"
 
 
-dict_ind_to_class = {0:'Pulling Hand In',2:'Swipe Left',1:'Swipe Right',3:'Thumb Up',4:'No Gesture'}
+if __name__ == "__main__":
 
-model1_path = "../Models/Model_84_5_jester"
-model1 = load_model(model1_path)
+    # Initialising dictionary
+    dict_ind_to_class = {0:'Pulling Hand In',1:'Swipe Right',2:'Swipe Left',3:'Thumb Up',4:'No Gesture'}
 
-font = cv2.FONT_HERSHEY_SIMPLEX
-quietMode = False
-img_rows,img_cols=64, 64 
-cap = cv2.VideoCapture(0)
-cv2.namedWindow('Original', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('Original',720,720)
+    #   Loading model
+    model1_path = "../Models/Model_84_5_jester"
+    model1 = load_model(model1_path)
 
+    # Setting up window
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    quietMode = False
+    img_rows,img_cols=64, 64 
+    cap = cv2.VideoCapture(0)
+    cv2.namedWindow('Original', cv2.WINDOW_NORMAL)
+    cv2.resizeWindow('Original',720,720)
 
-framecount = 0
-fps = ""
-start = time.time()
-frames = []
-num=[5]
-max =1
-real_index = 5
-instruction = 'No Gesture'
-pre =0
-prev2 = None
-prev = None
-black = np.zeros((100, 400, 3), dtype = "uint8")
+    # Initialising varibales
+    framecount = 0
+    fps = ""
+    start = time.time()
+    frames = []
+    num=[5]
+    max =1
+    real_index = 5
+    instruction = 'No Gesture'
+    pre =0
+    prev = None
+    prev2 = None
+    black = np.zeros((100, 400, 3), dtype = "uint8")
+    num_classes = 5
 
-num_classes = 5
-while(1):
-    
-    ret, frame = cap.read()
-    frame = cv2.flip(frame, 3)
-    frame = cv2.resize(frame, (640,480))
-    
-
-    framecount = framecount + 1
-    end  = time.time()
-    timediff = (end - start)
-    if( timediff >= 1):
-        fps = 'FPS:%s' %(framecount)
-        start = time.time()
-        framecount = 0
-
-    cv2.putText(frame,fps,(10,20), font, 0.7,(0,255,0),2,1)
-    X_tr=[]
-         
-    image=cv2.resize(frame,(img_rows,img_cols),interpolation=cv2.INTER_AREA)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    frames.append(gray)
-    input=np.array(frames)
-    
-    if input.shape[0]==16:
-        frames = frames[4:]
-        X_tr.append(input)
-        X_train= np.array(X_tr)
-        train_set = np.zeros((1, 16, img_cols,img_rows,3))
-        train_set[0][:][:][:][:]=X_train[0,:,:,:,:]
-        train_set = train_set.astype('float32')
-        train_set -= 108.26149
-        train_set /= 146.73851
-        result_1 = model1.predict(train_set)
-        # print(result_1)
-        num = np.argmax(result_1,axis =1)
-        instruction = dict_ind_to_class[num[0]]
-        if num[0]==prev and prev!=prev2:
-            if num[0]==0:
-                print(bring_down_tabs())
-            if num[0]==1:
-                print(minimizer())
-            if num[0]==2:
-                print(tabs_cycle())
-            if num[0]==3:
-                print(take_ss())
-        print("Curr Gesture : ",dict_ind_to_class[num[0]],"prev :",dict_ind_to_class[prev] if prev!=None else '000',"prev2 :",dict_ind_to_class[prev2] if prev2!=None else '-----')
-        prev2 = prev
-        prev = num[0]
+    # Running videostream
+    while(1):
         
-    cv2.putText(frame, instruction, (450, 50), font, 0.7, (0, 255, 0), 2, 1)
-    cv2.putText(black, "Quiet Mode  "+instruction, (0,50), font, 0.8, (255, 255, 255), 2, 1)
-    if not quietMode:
-        cv2.resizeWindow('Original',720,720)
-        cv2.imshow('Original',frame)
-    if quietMode:
-        cv2.resizeWindow('Original',100,400)
-        cv2.imshow('Original',black)
-    key = cv2.waitKey(1) & 0xFF
-    if key == 27:
-        break
-    elif key == ord('q'):
-        quietMode = not quietMode
-        minimizer()
-cap.release()
-cv2.destroyAllWindows()
+        ret, frame = cap.read()
+        frame = cv2.flip(frame, 3)
+        frame = cv2.resize(frame, (640,480))
+        
+
+        framecount = framecount + 1
+        end  = time.time()
+        timediff = (end - start)
+        if( timediff >= 1):
+            fps = 'FPS:%s' %(framecount)
+            start = time.time()
+            framecount = 0
+
+        cv2.putText(frame,fps,(10,20), font, 0.7,(0,255,0),2,1)
+        X_tr=[]
+             
+        image=cv2.resize(frame,(img_rows,img_cols),interpolation=cv2.INTER_AREA)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        frames.append(gray)
+        input=np.array(frames)
+        
+        if input.shape[0]==16:
+            frames = frames[4:]
+            X_tr.append(input)
+            X_train= np.array(X_tr)
+            train_set = np.zeros((1, 16, img_cols,img_rows,3))
+            train_set[0][:][:][:][:]=X_train[0,:,:,:,:]
+            train_set = train_set.astype('float32')
+            train_set -= 108.26149
+            train_set /= 146.73851
+            result_1 = model1.predict(train_set)
+            # print(result_1)
+            num = np.argmax(result_1,axis =1)
+            instruction = dict_ind_to_class[num[0]]
+            if num[0]==prev and prev!=prev2:
+                if num[0]==0:
+                    print(bring_down_tabs())
+                if num[0]==1:
+                    print(minimizer())
+                if num[0]==2:
+                    print(tabs_cycle())
+                if num[0]==3:
+                    print(take_ss())
+            print("Curr Gesture : ",dict_ind_to_class[num[0]],"prev :",dict_ind_to_class[prev] if prev!=None else '000',"prev2 :",dict_ind_to_class[prev2] if prev2!=None else '-----')
+            prev2 = prev
+            prev = num[0]
+            
+        cv2.putText(frame, instruction, (450, 50), font, 0.7, (0, 255, 0), 2, 1)
+        cv2.putText(black, "Quiet Mode  "+instruction, (0,50), font, 0.8, (255, 255, 255), 2, 1)
+        if not quietMode:
+            cv2.resizeWindow('Original',720,720)
+            cv2.imshow('Original',frame)
+        if quietMode:
+            cv2.resizeWindow('Original',100,400)
+            cv2.imshow('Original',black)
+        key = cv2.waitKey(1) & 0xFF
+        if key == 27:
+            break
+        elif key == ord('q'):
+            quietMode = not quietMode
+            minimizer()
+    cap.release()
+    cv2.destroyAllWindows()
     
 
 
